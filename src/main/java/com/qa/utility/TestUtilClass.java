93e30477d48d93bb247df59e7eedbbeaed85dc0e
unknown <birhade.pritesh@gmail.com>
2018-12-23 10:51:01 +0530

    First git commit for my selenium code

---
+++ src/main/java/com/qa/utility/TestUtilClass.java
@@ -0,0 +1,13 @@
+package com.qa.utility;
+
+public class TestUtilClass {
+	
+	public void testUtil(){
+		
+		System.out.println("Util class");
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,15 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void test(){
+		
+		System.out.println("Test test");
+		
+	}
+	
+
+}
